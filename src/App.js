37e51c94274eeb90e0b0e4c41718fc393f1d8432
mihaAM87 <MikhailAA1@yandex.ru>
2021-11-14 11:21:46 +0300

    0.1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react'
 //import 'react-select/dist/react-select.css'
 import {Route, Switch, Redirect, withRouter} from 'react-router-dom'
-import FolgerContentList from './components/folgerContent/folgerContentList'
+import screen from './components/screens/screen'
+import {RED, YELLOW, GREEN} from './store/actions/actionTypes';
 import './App.scss';
 
 class App extends Component {
   render() {
+
+    const {colorInterval, counter} = this.props;
+
     let routes = (
       <Switch>
-        <Route path="/source/source.json" component={FolgerContentList} />
+        <Route path="/green" component={screen} colorIndex={3} colorInterval={colorInterval} counter={counter}/>
+        <Route path="/yellow" component={screen} colorIndex={0} colorInterval={colorInterval} counter={counter} />
+        <Route path="/red" component={screen} colorIndex={1} colorInterval={colorInterval} counter={counter} />
       </Switch>)
     return (
-    <div className="App">
-      {routes}
-    </div>);
+      <div className="App">
+          {routes}
+        </div>
+      );
   }
 }
 
